6695fa8602497004216fd8cfed202330b4fecc3e
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:42:48 +0200

    Add Navbar and Notification components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Notification from "./components/Notification";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Notification />
+      <h1>StayHealthy Platform</h1>
+    </>
+  );
+}
+
+export default App;
 
