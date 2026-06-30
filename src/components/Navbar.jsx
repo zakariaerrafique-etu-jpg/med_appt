b5154b195309452173a66b6a742a34df140e8ec6
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:36:56 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+
+function Navbar() {
+  const handleLogout = () => {
+    localStorage.clear();
+    alert("Logged out");
+  };
+
+  return (
+    <nav>
+      <h2>StayHealthy</h2>
+      <ul>
+        <li>Home</li>
+        <li>Appointments</li>
+        <li>Sign Up</li>
+        <li>Login</li>
+        <li onClick={handleLogout}>Logout</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
 
