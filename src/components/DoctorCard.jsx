127370ab74eb9e8e7281d9ee262769c6dcf604e2
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:39:04 +0200

    Add DoctorCard component to display doctor details

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+
+function DoctorCard({ doctor, onCancel }) {
+  return (
+    <div>
+      <h3>{doctor.name}</h3>
+      <p>{doctor.speciality}</p>
+      <p>{doctor.experience} years</p>
+      <p>{doctor.rating}</p>
+
+      <button onClick={() => onCancel(doctor.id)}>
+        Cancel Appointment
+      </button>
+    </div>
+  );
+}
+
+export default DoctorCard;
 
