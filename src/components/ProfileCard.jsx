7744b250485291faa754669cfe14888f24a75f70
ERRAFIQUE ZAKARIA <zakaria.errafique-etu@etu.univh2c.ma>
2026-06-30 19:41:04 +0200

    Add ProfileCard component with input fields

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,1 +1,17 @@
+import React, { useState } from "react";
+
+function ProfileCard() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  return (
+    <div>
+      <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+      <input placeholder="Phone" onChange={(e) => setPhone(e.target.value)} />
+      <button>Save</button>
+    </div>
+  );
+}
+
+export default ProfileCard;
 
